4a126ecea555a3ec2f93cd80745c8022926378f0
Ryan Young <ryoung@brandeis.edu>
2024-05-01 01:49:46 -0400

    ability to process folder input

--- github2file/g2f.py
+++ github2file/g2f.py
@@ -164,9 +164,44 @@ def process_zip_file_object(zip_file, args):
             outfile.write(file_content)
             outfile.write("\n\n")
 
+
+def process_folder(args):
+    """Process files from a local folder."""
+    for root, _, files in os.walk(args.folder):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if (not is_file_type(file_path, args.lang)
+                or not any(is_likely_useful_file(file_path, lang) for lang in args.lang)
+                or should_exclude_file(file_path, args.exclude)):
+                continue
+
+            if args.include:
+                confirm = any(include in file_path for include in args.include)
+                if not confirm:
+                    logging.debug(f"Skipping file: {file_path}")
+                    continue
+
+            with open(file_path, 'r', encoding='utf-8') as f:
+                file_content = f.read()
+
+            if any(is_test_file(file_content, lang) for lang in args.lang) or not has_sufficient_content(file_content):
+                continue
+            if "python" in args.lang and not args.keep_comments:
+                try:
+                    file_content = remove_comments_and_docstrings(file_content)
+                except SyntaxError:
+                    continue
+
+            with open(args.output_file, 'a', encoding='utf-8') as outfile:
+                comment_prefix = "// " if any(lang in ["go", "js"] for lang in args.lang) else "# "
+                outfile.write(f"{comment_prefix}File: {file_path}\n")
+                outfile.write(file_content)
+                outfile.write("\n\n")
+
 def create_argument_parser():
     parser = argparse.ArgumentParser(description='Download and process files from a GitHub repository.')
     parser.add_argument('--zip_file', type=str, help='Path to the local .zip file')
+    parser.add_argument('--folder', type=str, help='Path to the local folder')
     parser.add_argument('--lang', type=str, default='python', help='The programming language(s) of the repository (comma-separated)')
     parser.add_argument('--keep-comments', action='store_true', help='Keep comments and docstrings in the source code (only applicable for Python)')
     parser.add_argument('--branch_or_tag', type=str, help='The branch or tag of the repository to download', default="master")
@@ -179,34 +214,38 @@ def create_argument_parser():
 
 def main(args=None):
     parser = create_argument_parser()
+    args = parser.parse_args(args)
+    args.lang = [lang.strip() for lang in args.lang.split(',')]
+    if args.include:
+        args.include = [subfolder.strip() for subfolder in args.include.split(',')]
+    if args.exclude:
+        args.exclude = [pattern.strip() for pattern in args.exclude.split(',')]
+    else:
+        args.exclude = []
+
+    if args.debug:
+        print("Debug logging enabled")
+        # Enable debug logging
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("Debug logging enabled")
+        logging.debug(f"Arguments: {args}")
+    else:
+        # Enable info logging
+        logging.basicConfig(level=logging.INFO)
+
     try:
-        args = parser.parse_args(args)
-
-
-        args.lang = [lang.strip() for lang in args.lang.split(',')]
-        if args.include:
-            args.include = [subfolder.strip() for subfolder in args.include.split(',')]
-        if args.exclude:
-            args.exclude = [pattern.strip() for pattern in args.exclude.split(',')]
-        else:
-            args.exclude = []
-
-        if args.debug:
-            print("Debug logging enabled")
-            # Enable debug logging
-            logging.basicConfig(level=logging.DEBUG)
-            logging.debug("Debug logging enabled")
-            logging.debug(f"Arguments: {args}")
-        else:
-            # Enable info logging
-            logging.basicConfig(level=logging.INFO)
-
         if args.repo_url:
             args.output_file = f"{args.repo_url.split('/')[-1]}_{','.join(args.lang)}.txt"
             download_repo(args)
-        else:
+        elif args.zip_file:
             args.output_file = f"{os.path.splitext(os.path.basename(args.zip_file))[0]}_{','.join(args.lang)}.txt"
             process_zip_file(args)
+        elif args.folder:
+            args.output_file = f"{os.path.basename(args.folder)}_{','.join(args.lang)}.txt"
+            process_folder(args)
+        else:
+            parser.print_help()
+            sys.exit(1)
 
         print(f"Combined {', '.join(args.lang).capitalize()} source code saved to {args.output_file}")
 
