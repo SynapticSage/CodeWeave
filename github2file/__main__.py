d4cdc9fd87428f3981efc2dc38183c4719f3d8f6
Ryan Young <ryoung@brandeis.edu>
2024-05-08 16:00:56 -0400

    beginning to reorg and simplify greatly

--- github2file/__main__.py
+++ github2file/__main__.py
@@ -1,24 +1,16 @@
-import sys
 import argparse
 import logging
 
 def main():
     parser = argparse.ArgumentParser(description="Download and process files from a GitHub repository.")
-    parser.add_argument("script", type=str, help="The script to run",
-                        choices=["g2f", "gui", "ts-js-rust"],
-                        default="g2f", nargs='?')
-    args, remaining_args = parser.parse_known_args()
-    logging.debug(f"Running {args.script} with arguments: {remaining_args}")
+    # parser.add_argument("script", type=str, help="The script to run",
+    #                     choices=["g2f", "gui", "ts-js-rust"],
+    #                     default="g2f", nargs='?')
+    _, remaining_args = parser.parse_known_args()
+    logging.debug(f"Running with arguments: {remaining_args}")
 
-    if args.script == "gui":
-        from github2file.gui import main as gui_main
-        gui_main(remaining_args)
-    elif args.script == "ts-js-rust":
-        from github2file.ts_js_rust2file import main as ts_js_rust_main
-        ts_js_rust_main(remaining_args)
-    else:
-        from github2file.g2f import main as g2f_main
-        g2f_main(remaining_args)  # Pass all arguments after the 'g2f' token
+    from github2file.g2f import main as g2f_main
+    g2f_main(remaining_args)  # Pass all arguments after the 'g2f' token
 
 if __name__ == "__main__":
     main()
--- github2file/g2f.py
+++ github2file/g2f.py
@@ -3,131 +3,12 @@ import sys
 import requests
 import zipfile
 import io
-import ast
 import logging
 import argparse
-import fnmatch
-import time
 
-file_extension_dict = {
-        'python': ['.py'],
-        'py': ['.py'],
-        'ipython': ['.ipynb'],
-        'ipynb': ['.ipynb'],
-        'go': ['.go'],
-        'js': ['.js'],
-        'html': ['.html'],
-        'mojo': ['.mojo'],
-        'java': ['.java'],
-        'c': ['.c','.h'],
-        'cpp': ['.cpp','.h','.hpp'],
-        'c++': ['.cpp','.h','.hpp'],
-        'csharp': ['.cs'],
-        'ruby': ['.rb'],
-        'mojo': ['.mojo'],
-        'javascript': ['.js'],
-        'markdown': ['.md', '.markdown', '.mdx'],
-        'md': ['.md'],
-        'shell': ['.sh'],
-        'bash': ['.sh'],
-        'zsh': ['.sh'],
-}
+from github2file.utils.path import should_exclude_file, inclusion_violate, extract_git_folder, is_test_file, is_file_type, is_likely_useful_file
+from github2file.utils.file import has_sufficient_content, remove_comments_and_docstrings
 
-def is_file_type(file_path, file_languages:list):
-    """Check if the file has any of the specified file extensions."""
-    is_ft = any(file_path.endswith(ext) for file_language in file_languages for
-        ext in file_extension_dict[file_language.replace('.','')])
-    if not is_ft:
-        logging.debug(f"Skipping file: {file_path}")
-    return is_ft
-
-def is_likely_useful_file(file_path:str, lang:str, args:argparse.Namespace)->bool:
-    """Determine if the file is likely to be useful by excluding certain
-    directories and specific file types."""
-    excluded_dirs = args.excluded_dirs
-    utility_or_config_files = []
-    github_workflow_or_docs = [".github", ".gitignore", "LICENSE", "README"]
-
-    if lang == "python" or lang == "mojo":
-        excluded_dirs.append("__pycache__")
-        utility_or_config_files.extend(["hubconf.py", "setup.py"])
-        github_workflow_or_docs.extend(["stale.py", "gen-card-", "write_model_card"])
-    elif lang == "go":
-        excluded_dirs.append("vendor")
-        utility_or_config_files.extend(["go.mod", "go.sum", "Makefile"])
-    elif lang == "js":
-        excluded_dirs.extend(["node_modules", "dist", "build"])
-        utility_or_config_files.extend(["package.json", "package-lock.json", "webpack.config.js"])
-    elif lang == "html":
-        excluded_dirs.extend(["css", "js", "images", "fonts"])
-
-    if any((part.startswith('.') and not part.startswith('..') and part != '.' and part != '..')
-        for part in file_path.split('/')):
-        logging.debug(f"Skipping hidden file: {file_path}")
-        return False
-    if 'test' in file_path.lower():
-        logging.debug(f"Skipping test file: {file_path}")
-        return False
-    for excluded_dir in excluded_dirs:
-        if f"/{excluded_dir}/" in file_path or file_path.startswith(excluded_dir + "/"):
-            logging.debug(f"Skipping excluded directory: {file_path}")
-            return False
-    for file_name in utility_or_config_files:
-        if file_name in file_path:
-            logging.debug(f"Skipping utility or config file: {file_path}")
-            return False
-    for doc_file in github_workflow_or_docs:
-        doc_file_check = (doc_file in file_path if not doc_file.startswith(".") else
-                 doc_file in os.path.basename(file_path))
-        if doc_file_check:
-            logging.debug(f"Skipping GitHub workflow or documentation file: {file_path}")
-            return False
-    return True
-
-def is_test_file(file_content, lang):
-    """Determine if the file content suggests it is a test file."""
-    test_indicators = []
-    if lang == "python" or lang == "mojo":
-        test_indicators = ["import unittest", "import pytest", "from unittest", "from pytest"]
-    elif lang == "go":
-        test_indicators = ["import testing", "func Test"]
-    elif lang == "js":
-        test_indicators = ["describe(", "it(", "test(", "expect(", "jest", "mocha"]
-    return any(indicator in file_content for indicator in test_indicators)
-
-def has_sufficient_content(file_content, min_line_count=10):
-    """Check if the file has a minimum number of substantive lines."""
-    lines = [line for line in file_content.split('\n') if line.strip() and not line.strip().startswith(('#', '//'))]
-    return len(lines) >= min_line_count
-
-def remove_comments_and_docstrings(source):
-    """Remove comments and docstrings from the Python source code."""
-    tree = ast.parse(source)
-    for node in ast.walk(tree):
-        if isinstance(node, (ast.FunctionDef, ast.ClassDef, ast.AsyncFunctionDef)) and ast.get_docstring(node):
-            node.body = node.body[1:]  # Remove docstring
-        elif isinstance(node, ast.Expr) and isinstance(node.value, ast.Str):
-            node.value.s = ""  # Remove comments
-    return ast.unparse(tree)
-
-def extract_git_folder(folder:str)->str|None:
-    """ Extract the git folder name from the folder path 
-    We must search from the lower child folder up to the parent folder
-    looking for .git
-    """
-    while folder:
-        if '.git' in os.listdir(folder):
-            return os.path.basename(folder)
-        folder = os.path.dirname(folder)
-    return None
-
-
-def should_exclude_file(file_path, exclude_patterns):
-    """Check if the file path matches any of the exclude patterns."""
-    answer = any(fnmatch.fnmatch(file_path, pattern) for pattern in exclude_patterns)
-    if answer:
-        logging.debug(f"Excluding file: {file_path}")
-    return answer
 
 def download_repo(args):
     """Download and process files from a GitHub repository."""
@@ -160,10 +41,12 @@ def process_zip_file_object(zip_file, args):
                 continue
 
             if args.include:
-                confirm = any(include in file_path for include in args.include)
+                confirm_include = any(include in file_path for include in args.include)
                 if not confirm:
                     logging.debug(f"Skipping file: {file_path}")
                     continue
+            if not confirm_include:
+                pass
 
             file_content = zip_file.read(file_path).decode("utf-8")
 
@@ -188,14 +71,11 @@ def process_folder(args):
             file_path = os.path.join(root, file)
             if (not is_file_type(file_path, args.lang)
                 or not any(is_likely_useful_file(file_path, lang, args) for lang in args.lang)
-                or should_exclude_file(file_path, args.exclude)):
+                or should_exclude_file(file_path, args)):
                 continue
 
-            if args.include:
-                confirm = any(include in file_path for include in args.include)
-                if not confirm:
-                    logging.debug(f"Skipping file: {file_path}")
-                    continue
+            if inclusion_violate(file_path, args):
+                continue
 
             with open(file_path, 'r', encoding='utf-8') as f:
                 file_content = f.read()
@@ -228,6 +108,7 @@ def create_argument_parser():
                         default="docs,examples,tests,test,scripts,utils,benchmarks")
     parser.add_argument('repo_url', type=str, help='The URL of the GitHub repository',
                         default="", nargs='?')
+    parser.add_argument('--name_append', type=str, help='Append this string to the output file name')
     return parser
 
 def check_for_include_override(include_list, exclude_list):
@@ -283,7 +164,10 @@ def main(args=None):
             if not gitfolder:
                 print("No git folder found in the path")
                 sys.exit(1)
-            args.output_file = f"{gitfolder}_{','.join(args.lang)}.txt"
+            if args.name_append:
+                args.output_file = f"{gitfolder}_{args.name_append}_{','.join(args.lang)}.txt"
+            else:
+                args.output_file = f"{gitfolder}_{','.join(args.lang)}.txt"
             process_folder(args)
         else:
             parser.print_help()
